4488b0c96900cec8dadc507fad3246303bab7bcb
Cody Yantis <cyantis@gmail.com>
2019-03-22 02:02:55 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -9,32 +9,25 @@ class Transfer
   end
 
   def valid?
-    sender.valid? && receiver.valid?
+    @sender.valid? && @receiver.valid?
   end
 
   def execute_transaction
-    if valid? && sender.balance > amount && status == "pending"
-      sender.balance -= amount
-      receiver.balance += amount
-      status = "complete"
+    if valid? && @status != "complete"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
     else
-      reject_transfer
+      "Transaction rejected. Please check your account balance."
     end
   end
 
   def reverse_transfer
-    if status == "complete"
-      sender.balance += amount
-      receiver.balance -= amount
-      status = "reversed"
-    else
-      reject_transfer
+    if @status == "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"
     end
-  end
-
-  def reject_transfer
-    status = "rejected"
-    "Transaction rejected. Please check your account balance."
   end
 
 end
