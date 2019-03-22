019dd45ed9ca4924ca641c1c1a300dd31facf4d5
Cody Yantis <cyantis@gmail.com>
2019-03-22 02:13:18 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,41 +1,40 @@
-class Transfer
+lass Transfer
+  attr_reader :amount, :sender, :receiver
   attr_accessor :status
-  attr_reader :sender, :receiver, :amount
 
   def initialize(sender, receiver, amount)
+    @status = "pending"
     @sender = sender
     @receiver = receiver
-    @status = "pending"
     @amount = amount
   end
 
   def valid?
-    @sender.valid? && @receiver.valid?
+    sender.valid? && receiver.valid?
   end
 
   def execute_transaction
-    if valid? && @sender.balance > @amount && @status == "pending"
-      @sender.balance -= @amount
-      @receiver.balance += @amount
-      @status = "complete"
+    if valid? && sender.balance > amount && self.status == "pending"
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status = "complete"
     else
-      reject
+      reject_transfer
     end
   end
 
   def reverse_transfer
-    if valid? && @receiver.balance > amount && self.status == "complete"
-      @sender.balance += @amount
-      @receiver.balance -= @amount
-      @status = "reversed"
+    if valid? && receiver.balance > amount && self.status == "complete"
+      receiver.balance -= amount
+      sender.balance += amount
+      self.status = "reversed"
     else
-      reject
+      reject_transfer
     end
   end
 
-  def reject
-    @status = "pending"
+  def reject_transfer
+    self.status = "rejected"
     "Transaction rejected. Please check your account balance."
   end
-
 end
