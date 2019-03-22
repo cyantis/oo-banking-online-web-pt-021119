5c2c120b5d7a1f8a591e6a9e8d2ca5636cf02f4c
Cody Yantis <cyantis@gmail.com>
2019-03-22 01:48:33 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
   end
 
   def execute_transaction
-    if valid?
+    if valid? && @status != "complete"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
